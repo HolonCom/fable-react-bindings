1eafa82f3f246909ff14ce353e7a96ec43d8ccdf
jannesiera <janne.siera@gmail.com>
2019-08-22 15:30:54 +0200

    Initialize with fable2-samples/minimal

---
+++ src/App.fs
@@ -0,0 +1,43 @@
+module App
+
+(**
+ The famous Increment/Decrement ported from Elm.
+ You can find more info about Elmish architecture and samples at https://elmish.github.io/
+*)
+
+open Elmish
+open Elmish.React
+open Fable.React
+open Fable.React.Props
+
+// MODEL
+
+type Model = int
+
+type Msg =
+| Increment
+| Decrement
+
+let init() : Model = 0
+
+// UPDATE
+
+let update (msg:Msg) (model:Model) =
+    match msg with
+    | Increment -> model + 1
+    | Decrement -> model - 1
+
+// VIEW (rendered with React)
+
+let view (model:Model) dispatch =
+
+  div []
+      [ button [ OnClick (fun _ -> dispatch Increment) ] [ str "+" ]
+        div [] [ str (string model) ]
+        button [ OnClick (fun _ -> dispatch Decrement) ] [ str "-" ] ]
+
+// App
+Program.mkSimple init update view
+|> Program.withReactSynchronous "elmish-app"
+|> Program.withConsoleTrace
+|> Program.run
